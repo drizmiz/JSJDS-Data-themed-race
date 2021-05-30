068a2f0e5bd39019d3a009d3a74116ae6241fa99
Honour-Van <fhn037@126.com>
2021-05-30 22:09:58 +0800

    :sparkles:life expectancy visualized

--- visualization/health.py
+++ visualization/health.py
@@ -9,29 +9,25 @@ tl = Timeline(init_opts=opts.InitOpts(
     bg_color='white',
     page_title='预期寿命'
 ))
-with open("./country_ce.json", 'r', encoding='utf-8') as f:
-    ce_dict = json.load(f)
+with open("./data/syno_dict.json", 'r', encoding='utf-8') as f:
+    syno_dict = json.load(f)
 
-df = pd.read_csv('./FDI_filled_m.csv')
-df.iloc[:, 3] = df.iloc[:, 3].apply(np.log1p)
-for year in range(2003, 2019+1):
+df = pd.read_csv('./data/LE.csv')
+for year in [2000, 2010, 2015, 2019]:
     map = (
         Map()
         .add(df.columns.tolist()[-1],
-             [[ce_dict[row['国家']], row[3]]
-                 for _, row in df[df.iloc[:, 0] == year].iterrows()],
+             [[row[0], row[-1]]
+                 for _, row in df[(df['Period'] == year) & (df['Dim1'] == "Both sexes")].iterrows()],
              maptype="world",
              is_map_symbol_show=False,  # 不描点
              )
         .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
         .set_global_opts(
-            title_opts=opts.TitleOpts(title=f"{year}年外商直接投资情况"),
-            visualmap_opts=opts.VisualMapOpts(
-                max_=df[df.iloc[:, 0] == year].iloc[:, 3].max()),
-            toolbox_opts=opts.ToolboxOpts(),
-
+            title_opts=opts.TitleOpts(title=f"{year}年预期寿命"),
+            visualmap_opts=opts.VisualMapOpts(min_=df['First Tooltip'].min(),max_=df['First Tooltip'].max()),
+            toolbox_opts=opts.ToolboxOpts()
         )
     )
-    tl.add(map, f"{year}年")
-# tl.render("vis1.html")
-tl.render("vis2.html")
+    tl.add(map, f"{year}")
+tl.render("./out/life_expectancy.html")

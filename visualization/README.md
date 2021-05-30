7b922c52859dfe4c36d9531ec1942c2fb4b6f671
Honour-Van <fhn037@126.com>
2021-05-30 11:23:59 +0800

    :sparkles:visualization 1 finished based on FDI

---
+++ visualization/README.md
@@ -0,0 +1,6 @@
+# visualization workspace
+- `mytool.ipynb`: some tests, and do some chores here
+- `world_country.json`: raw English-Chinese dict downloaded from website
+- `country_ce.json`: a look-up dict modified based on our datasets. some more keys addeed or revised 
+- `FDI_useful.csv`: dataset modified to delet HK, Macau and Taiwan. Since we believe they are inalienable parts of China, corresponding with API of echarts.
+- `heatmap.py`: used to generate the heatmap with timeline
---
+++ visualization/heatmap.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import json
+from pyecharts import options as opts
+from pyecharts.charts import Timeline, Map
+tl = Timeline()
+with open("./country_ce.json", 'r', encoding='utf-8') as f:
+    ce_dict = json.load(f)
+
+df = pd.read_csv('./FDI_useful.csv')
+for year in range(2003, 2019+1):
+    map = (
+        Map()
+        .add(df.columns.tolist()[-1],
+             [[ce_dict[row['国家']], row[3]]
+                 for _, row in df[df.iloc[:, 0] == year].iterrows()],
+             maptype="world",
+             is_map_symbol_show=False,  # 不描点
+             )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title=f"{year}年外商直接投资情况"),
+            visualmap_opts=opts.VisualMapOpts(is_piecewise=True),
+        )
+    )
+    tl.add(map, f"{year}年")
+tl.render("vis1.html")

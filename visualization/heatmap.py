d740adab24877a3c618b5aee46cc9f465d79085f
Honour-Van <fhn037@126.com>
2021-05-30 14:30:33 +0800

    :art:do some art

--- visualization/heatmap.py
+++ visualization/heatmap.py
@@ -2,17 +2,22 @@ import pandas as pd
 import json
 from pyecharts import options as opts
 from pyecharts.charts import Timeline, Map
+from pyecharts.globals import ThemeType
 import numpy as np
 tl = Timeline()
 with open("./country_ce.json", 'r', encoding='utf-8') as f:
     ce_dict = json.load(f)
 
 df = pd.read_csv('./FDI_useful.csv')
-df.iloc[:,3] = df.iloc[:,3].apply(np.log1p)
+df.iloc[:, 3] = df.iloc[:, 3].apply(np.log1p)
 for year in range(2003, 2019+1):
     map = (
-        Map()
-        .add(df.columns.tolist()[-1],
+        Map(init_opts=opts.InitOpts(
+            theme=ThemeType.INFOGRAPHIC,
+            bg_color='white',
+            page_title='外商直接投资情况'
+        ))
+        .add(df.columns.tolist()[-1]+"（对数值，原单位：百万美元）",
              [[ce_dict[row['国家']], row[3]]
                  for _, row in df[df.iloc[:, 0] == year].iterrows()],
              maptype="world",
@@ -22,7 +27,9 @@ for year in range(2003, 2019+1):
         .set_global_opts(
             title_opts=opts.TitleOpts(title=f"{year}年外商直接投资情况"),
             visualmap_opts=opts.VisualMapOpts(
-                max_=df[df.iloc[:, 0] == year].iloc[:,3].max(), is_piecewise=True),
+                max_=df[df.iloc[:, 0] == year].iloc[:, 3].max()),
+            toolbox_opts=opts.ToolboxOpts(),
+
         )
     )
     tl.add(map, f"{year}年")

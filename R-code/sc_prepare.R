a9764460202829aedf7616a37ddb752fc3f6cde7
drizzle <drizmiz@outlook.com>
2021-05-31 17:41:58 +0800

    fix collaborator name; infant mortality data added

--- R-code/sc_prepare.R
+++ R-code/sc_prepare.R
@@ -5,14 +5,29 @@ library(tidyverse)
 
 source("math.R")
 
-# Reformat data
+# Reformat FDI data
 
-dat <- read_csv("../data/investment/FDI_filled.csv")
+dat1 <- read_csv("../data/investment/FDI_filled.csv")
 
-dat <- dat %>%
+dat1 <- dat1 %>%
   mutate(lg = robust_log(对外直接投资), .keep = "unused") %>%
   select(-地区) %>%
   pivot_wider(names_from = "国家", values_from = "lg") %>%
+  arrange(年份) %>%
   select(-年份)
 
-dat %>% write_csv("../data/investment/FDI_for_sc.csv")
+dat1 %>% write_csv("../data/investment/FDI_for_sc.csv")
+
+# Reformat infant mortality data
+
+dat2 <- read_csv("../data/world_health/tidy/iMR_tidy_zh.csv")
+
+dat2 <- dat2 %>%
+  mutate(lg = robust_log(infantMortalityRate), .keep = "unused") %>%
+  select(-country) %>%
+  filter(国家 %>% is.na() %>% `!`, 年份 >= 2001, 国家 != "苏丹") %>%
+  pivot_wider(names_from = "国家", values_from = "lg") %>%
+  arrange(年份) %>%
+  select(-年份)
+
+dat2 %>% write_csv("../data/world_health/tidy/iMR_for_sc.csv")

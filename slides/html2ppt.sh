e2b83820ab6236f70ef9fb117440b3b289ab9c36
drizzle <drizmiz@outlook.com>
2021-07-06 00:37:19 +0800

    update vis

--- slides/html2ppt.sh
+++ slides/html2ppt.sh
@@ -1,7 +1,9 @@
 
+R -e 'pagedown::chrome_print("slides.html")'
+
 mkdir ./temp_jpg/
 gs -dNOPAUSE -dBATCH -dSAFER -dGraphicsAlphaBits=4 -dTextAlphaBits=4 -sDEVICE=jpeg -r300 -sOutputFile='temp_jpg/page-%00d.jpg' slides.pdf
 
-read -p "Open LibreOffice to generate the pdf from jpg. Then press any key to resume..."
+read -p "Open LibreOffice to generate ppt from jpg. Then press [ENTER] to resume..."
 
 rm --recursive ./temp_jpg/
--- slides/update_res.sh
+++ slides/update_res.sh
@@ -1,7 +1,12 @@
 
-cd ./resources
-cp ../../document/resources/* .
+cd ./resources/
+cp ../../document/resources/* ./
+rm ./*.{bib,csl,tex}
 
 for f in *.pdf; do
   pdf2svg $f ${f%%.*}.svg
 done
+
+rm ./*.pdf
+
+cp ../../visualization/out/* ./html/

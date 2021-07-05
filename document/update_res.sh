4677bc3e0af3c5a4709466954557f881f0d1a1a5
drizzle <drizmiz@outlook.com>
2021-07-05 21:11:20 +0800

    update

--- document/update_res.sh
+++ document/update_res.sh
@@ -1,4 +1,5 @@
 
 cp --archive --force ./new_res/* ./resources/
 cp --force ../results/IMR/pooled/pooled_average.pdf ./resources/IMR_pooled.pdf
-
+cp --force ../results/5MR/pooled/pooled_average.pdf ./resources/5MR_pooled.pdf
+cp --force "../results/IMR/黑山_ci_sc.pdf" ./resources/
